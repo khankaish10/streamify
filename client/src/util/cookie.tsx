a838d6f474836febe0acdc3e0f0e1bfa01ed8003
khankaish <khankaish10@gmail.com>
2025-05-05 12:15:01 +0530

    removed cookie from server

--- client/src/util/cookie.tsx
+++
@@ -1,9 +0,0 @@
-
-'use server'
-import { cookies } from 'next/headers'
- 
-export default async function getCookie() {
-  const cookieStore = await cookies()
-  const theme = cookieStore.get('accessToken')
-  return theme
-}
