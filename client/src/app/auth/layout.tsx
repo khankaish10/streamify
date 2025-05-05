e34a012a04c76fc21456541e792f691bb4ef0684
khankaish <khankaish10@gmail.com>
2025-05-05 14:51:53 +0530

    redirect to home if user already logged in

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+'use client'
+import React, {useEffect, useState} from 'react'
+import { useRouter } from 'next/navigation';
 
 const AuthLayout = ({ children }: Readonly<{
   children: React.ReactNode;
 }>) => {
+
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    const user = localStorage.getItem('user')
+    if(user && JSON.parse(user)?.accessToken) {
+      router.replace('/')
+    } else {
+      setLoading(false)
+    }
+  },[router])
+
+  if(loading) return null;
+
   return (
     <div className='flex h-screen bg-gray-100'>
       <div className='.left-video hidden md:flex flex-1 justify-center 
