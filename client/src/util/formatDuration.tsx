b6ffe2879e8e1812248f86d1588f8b34643f2331
khankaish <khankaish10@gmail.com>
2025-05-08 13:07:28 +0530

    type change

--- client/src/util/formatDuration.tsx
+++ client/src/util/formatDuration.tsx
@@ -1,10 +1,17 @@
 import moment from 'moment'
 import 'moment-duration-format'
 
+declare module 'moment' {
+  interface Duration {
+    format: (template: string, options?:{trim?:boolean}) => string
+  }
+}
+
 const formatDuration = (seconds: number) => {
     if(seconds < 1) return `${seconds.toFixed(1)}s`
 
-    return moment.duration(seconds, 'seconds')?.format('m:ss', { trim: false})
+    const duration = moment.duration(seconds, 'seconds')
+    return duration.format('m:ss', { trim: false})
   }
 
 
